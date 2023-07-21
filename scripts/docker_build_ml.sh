7277f4148ebb8c70ad0d822a5ac5faf9a3cf15d4
Dylan Auty <dylan.auty@gmail.com>
2023-07-21 15:59:05 +0100

    Version number

--- scripts/docker_build_ml.sh
+++ scripts/docker_build_ml.sh
@@ -5,7 +5,7 @@ set -e
 source scripts/docker_base.sh
 source scripts/opencv_version.sh
 source scripts/python_version.sh
-source scripts/docker_build_opencv.sh
+source scripts/docker_build_opencv.sh "4.5.0"
 
 CONTAINERS=${1:-"all"}
 
